0afd4ff37506bbff16fa840ca6dd1c59f090a8a0
turostaska <rado.albert@gmail.com>
2021-04-19 22:09:53 +0200

    milánói makarónixddd

--- server.py
+++ server.py
@@ -1,5 +1,59 @@
-def print_hi(name):
-    print(f'Most már az irl beszélgetrsekben is jelen van {name}!')
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Hash import SHA256
+
+from utils import *
+import os.path
+from Crypto.PublicKey import RSA
+
+
+private_key_path = './private-key'
+
+
+key_pair = None
+
+netif = init_network(server_addr)
+
+def auth_client(ciphertext):
+    cipher = PKCS1_OAEP.new(key_pair)
+    data = cipher.decrypt(ciphertext)
+
+    type = data[:1]
+    if type != types['session_init']:
+        print("Invalid message in this state! Awaiting session_init!")
+        return
+
+    len = data[1:2]
+    if int.from_bytes(len, "big") != 98:
+        print("Invalid message length")
+        return
+
+    username_padded = data[2:34]
+    password_padded = data[34:66]
+    session_key_padded = data[66:98]
+
+    username = remove_padding(username_padded)
+    password = remove_padding(password_padded)
+    session_key = remove_padding(session_key_padded)
+
+    print(username.decode('utf8'))
+    print(password.decode('utf8'))
+    print(session_key)
+
+    hash = SHA256.new()
+    hash.update(password)
+    password_hashed = hash.digest()
+    print(users[username.decode('utf8')] == password_hashed)
+
+    # header = types['session_init'] + bytes([98])
+    # body = pad_to_length(username, 32) + pad_to_length(password, 32)
+    #
+    # key = RSA.generate(2048)
+    #
+    # ciphertext = cipher.encrypt(header + body)
+    #
+    # with open(in_dir + '/' + msgs[self.last_read + 1], 'rb') as f:
+    #     msg = f.read()
+    pass
 
 
 def generate_rsa_keys():
@@ -37,4 +91,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print_hi('Amy')
+    main()
+
